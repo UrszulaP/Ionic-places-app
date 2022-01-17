06c8dff4df4ed56cde8b888fcbd2886830d74b52
UrszulaP <urszulapoliszuk@gmail.com>
2022-01-17 19:38:04 +0100

    get place in place detail page

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,15 +11,26 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  place: Place;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placesService: PlacesService
+    ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
   
   onBookPlace() {
-    // this.router.navigateByUrl('/places/tabs/discover'); // Angular way, plays forward animation by default
-    // this.navCtrl.pop(); // goes back by removing page from Pages Stack, doesn't work when page loaded directly as a first stack item
-    this.navCtrl.navigateBack('/places/tabs/discover'); // backward animation
+    this.navCtrl.navigateBack('/places/tabs/discover');
   }
 
 }
