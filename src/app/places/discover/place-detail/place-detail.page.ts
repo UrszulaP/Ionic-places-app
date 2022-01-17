ada87226f814bd020038a50ef0a9ba68e187c545
UrszulaP <urszulapoliszuk@gmail.com>
2022-01-17 18:50:15 +0100

    btn back navigation solutions

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+  
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover'); // Angular way, plays forward animation by default
+    // this.navCtrl.pop(); // goes back by removing page from Pages Stack, doesn't work when page loaded directly as a first stack item
+    this.navCtrl.navigateBack('/places/tabs/discover'); // backward animation
   }
 
 }
